72b1092eda94428e0ccff1eb634f7802e7a92958
mkim1994 <mkim2@andrew.cmu.edu>
2016-12-18 00:51:33 -0500

    preparing for builds real

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -12,7 +12,9 @@ public class GameMaster : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
 	}
 
 	public void LoadGame(){
